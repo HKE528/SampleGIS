1edb7444e4b6bbcf11640bae2a2abc52646edcc7
GEON_DEV_5_HKE <gosumo12@gmail.com>
2021-08-06 14:32:00 +0900

    Initial commit

---
+++ src/main/java/com/example/SampleGIS/SampleGisApplication.java
@@ -0,0 +1,13 @@
+package com.example.SampleGIS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleGisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleGisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SampleGIS/SampleGisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SampleGIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleGisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
