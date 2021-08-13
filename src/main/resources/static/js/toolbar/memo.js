73e0cac8afd99b9ece1de6d52076650fdb21905f
GEON_DEV_5_HKE <gosumo12@gmail.com>
2021-08-13 10:29:17 +0900

    메모 커서 오버레이 생성

--- src/main/resources/static/js/toolbar/memo.js
+++ src/main/resources/static/js/toolbar/memo.js
@@ -4,6 +4,45 @@ import {map} from "../map/resource.js"
 const memoBtn = createEl("utilMemo");
 memoBtn.addEventListener("click", btnEvent);
 
+const memoCursorOverlay = createCursor();
+map.addOverlay(memoCursorOverlay);
+
 function btnEvent(evt) {
     this.classList.toggle("active");
+
+    if (this.classList.contains("active")) {
+        memoStart();
+    } else {
+        memoComplete();
+    }
+}
+
+function memoStart() {
+    map.on('pointermove', pointerMoveHandler);
+}
+
+function memoComplete() {
+    memoBtn.classList.remove("active");
+    map.un('pointermove', pointerMoveHandler);
+
+    memoCursorOverlay.setPosition(null);
+}
+
+function pointerMoveHandler(evt) {
+    memoCursorOverlay.setPosition(evt.coordinate);
+}
+
+function createCursor() {
+    let cursorElement = document.createElement('div');
+    cursorElement.className = 'ol-tooltip ol-tooltip-measure';
+
+    let cursorOverlay = new ol.Overlay({
+        element: cursorElement,
+        offset: [0, -5],
+        positioning: 'bottom-center',
+        stopEvent: false,
+        insertFirst: false,
+    });
+
+    return cursorOverlay;
 }
